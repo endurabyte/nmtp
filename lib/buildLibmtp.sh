43768795f88d2404e122878123bceaf93bc13a25
Doug Slater <doug@slater.dev>
2023-11-13 13:12:44 -0500

    Use endurabyte fork of libmtp

--- lib/buildLibmtp.sh
+++ lib/buildLibmtp.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/shaosss/libmtp.git
+git clone https://github.com/endurabyte/libmtp.git
 cd libmtp/
-git checkout  master-libmtpsharp
+git checkout master-libmtpsharp
 
 printf 'n\n' | PKG_CONFIG_LIBDIR=$2/lib/pkgconfig/ LDFLAGS=-L$2/lib CPPFLAGS=-I$2/include ./autogen.sh --host=$1 --prefix=$2
 make
